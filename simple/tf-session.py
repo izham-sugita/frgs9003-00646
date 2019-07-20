c43bb7f808d5e01a9541c0de4a283ed4cbbf61a7
sugita <sugita5019@gmail.com>
2019-07-20 20:37:55 +0800

    new code

---
+++ simple/tf-session.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import os
+
+#remove verbrose messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+# Build a graph.
+a = tf.constant(5.0)
+b = tf.constant(6.0)
+c = a * b
+
+# Launch the graph in a session.
+sess = tf.compat.v1.Session()
+
+# Evaluate the tensor `c`.
+print(sess.run(c))
